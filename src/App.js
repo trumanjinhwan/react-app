49c49667a3c8ef6051eb75cc670e2fb9137ec0e4
82104 <trumanjinhwan@gmail.com>
2025-04-11 11:25:43 +0900

    init comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header(props) {
+  console.log(props);
+  return (
+    <header>
+      <h1>
+        <a href="/">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+  for(let i = 0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li>{t.title}</li>)
+  }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props){
+  return(
+    <article>
+        <h2>{props.title}</h2>
+        {props.body}
+      </article>
+  )
+}
 
 function App() {
+  const topics =[
+    {id:1, title: 'html', body: 'html is ...'},
+    {id:2, title: 'css', body: 'css is ...'},
+    {id:3, title: 'javascript', body: 'javascript is ...'}
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title = "WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title = "WELCOME" body = "Hello  WEB"></Article>
     </div>
   );
 }
